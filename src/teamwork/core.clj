c05fdf539deb3bc73ec4a5241eefacb1bb55209e
unknown <parulguptasmail@gmail.com>
2018-04-04 17:53:31 -0400

    First team work project initialised

---
+++ src/teamwork/core.clj
@@ -0,0 +1,31 @@
+(ns teamwork.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!")
+
+  (comment This app we will use to practise our clojure modules with repository access
+           Each person will create a new clojure file or namespace with code examples within this app
+           each clj file must have a function called run_examples that conditionally or sequentially calls
+           all other functions you created in your file to demonstrate the concept learnt.
+           We will then commit and push our individual cljs to the repo and by friday evening we will
+           try to run the main to see the results of each module .
+           I have taken the liberty to assign to each of us the topics which we must be confident with to be
+           ready for the clojure project.
+           Vishal - macros, defprotocol and extend-type etc
+           Arun   - file IO read write append, mapping say student name and class type atrributes from a file to a structure or map
+           Varshita - concurrency and parallelism in programming clojure book
+           Raja - Recursive function calls
+           Shiva  - specs and validations
+           Parul  - test driven development error handling
+
+           please code your run_examples function to take one integer argument that we will pass to it from -main
+           to selectively run our modules.
+           Once all of us have checked in our code n github we can review each others work to learn .
+
+           )
+
+
+  )
---
+++ test/teamwork/core_test.clj
@@ -0,0 +1,7 @@
+(ns teamwork.core-test
+  (:require [clojure.test :refer :all]
+            [teamwork.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
